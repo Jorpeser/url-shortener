6ffc53f9e0105686c5f828e2d08ed66d615cb6d5
Jorpeser <pepeperezvalenzuela@gmail.com>
2024-11-29 20:23:42 +0100

    Backend folder deletion

--- backend/src/util.js
+++
@@ -1,8 +0,0 @@
-/**
- * Function to check if an email is regex valid
- * @param {*} email 
- * @returns boolean
- */
-export function isEmail(email){
-  return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
-}
