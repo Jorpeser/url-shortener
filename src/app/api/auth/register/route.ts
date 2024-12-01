75ada4df151c0656bfba30d7a87cc78f51f52ec8
Jorpeser <pepeperezvalenzuela@gmail.com>
2024-12-01 17:16:41 +0100

    feat(register): add register api route

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+import { register } from "@/lib/controllers/auth.controller";
+
+export async function POST(request: NextRequest) {
+
+    const data = await request.json()
+    console.log("register data", data)
+    
+    if (!data) {
+        return NextResponse.json(
+            { error: "No data provided" }, 
+            { status: 400 }
+        )
+    }
+
+    const result = await register(data)
+
+    // Success response
+    if (result.status === 201) {
+        return NextResponse.json(
+            { message: result.message },
+            { status: result.status }
+        )
+    }
+
+    // Error response
+    return NextResponse.json(
+        { message: result.message },
+        { status: result.status }
+    )
+}
