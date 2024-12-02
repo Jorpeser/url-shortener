1ab707d907536b7233a72e2933e141ad140883b8
Jorpeser <pepeperezvalenzuela@gmail.com>
2024-12-02 10:30:41 +0100

    feat(auth): refactor registration and login functions to use session management and improve response handling

--- src/lib/controllers/auth.controller.ts
+++ src/lib/controllers/auth.controller.ts
@@ -1,53 +1,52 @@
+"use server"
+
 import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import { JWT_SECRET } from '../../../config'
 import { isEmail } from '../../utils'
 import { User } from '@/lib/models/User.model'
 import { connectDB } from '../db'
 import { NextResponse } from 'next/server'
+import { createSession, deleteSession, verifySession } from '../session'
 
-// [/] Function to handle user registration
-/*  Registering takes email password and username 
-    and creates a User DB item */
-export const register = async (req: Request, res: Response) => {
+// [X] Function to handle user registration
+export const register = async (data: any) => {
     try {
-        const { email, password, username } = await req.json()
+        const { email, password, username } = data
 
         await connectDB()
 
-        // Check if username already exists
+        // 1.- Check if username already exists
         const sameUsername = await User.findOne({
             username: username,
         })
         if (sameUsername) {
-            return NextResponse.json(
-                { message: 'Username already in use' },
-                { status: 400 }
-            )
+            return {
+                message: 'Username already in use',
+                status: 400
+            }
         }
 
-        // Check if email already exists
+        // 2.- Check if email already exists
         const sameEmail = await User.findOne({ email: email })
         if (sameEmail) {
-            return NextResponse.json(
-                { message: 'Email already in use' },
-                { status: 400 }
-            )
+            return {
+                message: 'Email already in use' ,
+                status: 400 
+            }
         }
 
-        // Check if password is valid
+        // 3.- Check if password is valid
         if (!/(?=.*\d)(?=.*[A-Z]).{8,}/.test(password)) {
-            return NextResponse.json(
-                { message: 'Password must be at least 8 characters long and contain at least one number and one capital letter'},
-                { status: 400 }
-            )
+            return {
+                 message: 'Password must be at least 8 characters long and contain at least one number and one capital letter',
+                 status: 400 
+            }
         }
 
-        // Hash the password
+        // 4.- Hash the password
         const hashedPassword = bcrypt.hashSync(password, 10)
 
-        // Create a new user
-        const newUser = await User.create({
+        // 5.- Create the new user
+        await User.create({
             email: email,
             password: hashedPassword,
             username: username,
@@ -55,111 +54,132 @@ export const register = async (req: Request, res: Response) => {
             urls: [],
         })
 
-        return NextResponse.json(
-            { message: 'User registered successfully'},
-            { status: 201 }
-        )
+        // 6.- Send success response
+        return {
+            message: 'User registered successfully',
+            status: 201 
+        }
+
     } catch (err) {
         console.error('Error creating user: ' + err)
-        return NextResponse.json(
-            { message: 'Error creating user' },
-            { status: 500 }
-        )
+        return {
+            message: 'Error creating user',
+            status: 500
+        }
     }
 }
 
 // [ ] Function to handle user login
-export const login = async (req: Request, res: Response) => {
+export const login = async (data: any) => {
     try {
-    
-        const { emailOrUsername, password } = await req.json()
+        const { emailOrUsername, password } = data
         
         await connectDB()
 
         let user
 
-        // Validate the provided email or username and find the user
+        // 1.- Validate the provided email or username and find the user
         if (isEmail(emailOrUsername)) {
             user = await User.findOne({ email: emailOrUsername })
-            console.log("Login con email: ", user)
         } else {
             user = await User.findOne({ username: emailOrUsername })
-            console.log("Login con username: ", user)
         }
 
-        // Check if the user exists
+        // 2.- Check if the user exists
         if (!user) {
-            return NextResponse.json(
-                { message: 'Invalid user and password combination' },
-                { status: 400 }
-            )
+            return {
+                message: 'Invalid user and password combination',
+                status: 400
+            }
         }
 
-        // Compare the provided password with the hashed password in the database
+        // 3.- Compare the provided password with the hashed password in the user database item
         const validPassword = await bcrypt.compare(password, user?.password)
         if (!validPassword) {
-            return NextResponse.json(
-                { message: 'Invalid user and password combination' },
-                { status: 400 }
-            )
+            return {
+                message: 'Invalid user and password combination',
+                status: 400
+            }
         }
 
-        // Generate a JWT token
-        const token = jwt.sign(
-            { id: user._id, username: user.username }, // JWT payload
-            JWT_SECRET, // Secret key
-            { expiresIn: '1h' }
-        )
+        // 4.- We create a session for the user and set a cookie
+        await createSession(user._id)
 
-        // Set the token as a cookie and send the response
-        res.cookie('auth_token', token, {
-            httpOnly: true, // Prevents client-side access to the cookie
-            secure: false, // NODE_ENV === 'production', // Only secure cookie in production
-            sameSite: 'none', // SameSite policy
-            credentials: 'include',
-            maxAge: 1000 * 60 * 60, // 1 hour expiration time
-        }).send({ user, token })
+        // 5.- Send success response
+        return {
+            message: 'User logged in successfully',
+            status: 200
+        }
+
     } catch (err) {
-        res.status(400).json({ message: err.message })
+        return {
+            message: 'Error logging in',
+            status: 500
+        }
     }
 }
 
 // [ ] Function to handle user logout
 export const logout = async (req: Request, res: Response) => {
-    res.clearCookie('auth_token').json({ message: 'Logged out' })
+    try {
+        // 1.- Delete the session
+        await deleteSession()
+
+        // 2.- Send success response
+        return NextResponse.json(
+            { message: 'Logged out successfully' },
+            { status: 200 }
+        )
+
+    } catch (err) {
+        console.error('Error logging out: ' + err)
+        return NextResponse.json(
+            { message: 'Error logging out' },
+            { status: 500 }
+        )
+    }
 }
 
+// [ ] Function to get the current user
 export const me = async (req: Request, res: Response) => {
-    const token = req.cookies.auth_token
-    console.log("Token: ", token)
-    //console.log("Request: ", req)
-    console.log("Cookies: ", req.cookies)
 
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' })
+    // 1.- Verify if the user is logged in
+    const session = await verifySession()
+    if (!session || 'json' in session) {
+        return NextResponse.json(
+            { message: 'Unauthorized' },
+            { status: 401 }
+        )
     }
 
     try {
-        const decoded = jwt.verify(token, JWT_SECRET)
-        const user = await User.findById(decoded.id).select('-password') // Excluir el campo de la contraseña
+        // 2.- Find the user in the database
+        const user = await User.findById(session.userId).select('-password') // Excluir el campo de la contraseña
+        
+        // X.- Return error if user not found
         if (!user) {
-            return res.status(404).json({ message: 'User not found' })
+            return NextResponse.json(
+                { message: 'User not found' },
+                { status: 404 }
+            )
         }
-        res.status(200).json({
-            id: user._id,
-            username: user.username,
-            email: user.email,
-            tier: user.tier,
-            urls: user.urls,
-            // Agrega cualquier otra información relevante del usuario
-        })
+
+        // 3.- Send success response and return the user data
+        return NextResponse.json(
+            {
+                id: user._id,
+                username: user.username,
+                tier: user.tier,
+                urls: user.urls,
+            },
+            { status: 200 }
+        )
+
     } catch (err) {
-        if (err.name === 'TokenExpiredError') {
-            return res.status(401).json({ message: 'Token expired' })
-        }
-        if (err.name === 'JsonWebTokenError') {
-            return res.status(401).json({ message: 'Invalid token' })
-        }
-        res.status(500).json({ message: 'Internal server error' })
+        console.error('Error getting user: ' + err)
+        return NextResponse.json(
+            { message: 'Error getting user' },
+            { status: 500 }
+        )
     }
 }
